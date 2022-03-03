01d345599a5701e0bebf8e02f0fa346614fdba4e
EC2 Default User <ec2-user@ip-172-31-44-157.ec2.internal>
2022-03-03 09:26:50 +0000

    [Update]8a-2

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,5 +2,5 @@ class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :room
 # 　チャットの文字数140字まで
-  validates :message,presence:true,length:{maximum:140}
+  validates :message, presence: true, length: { maximum:140 }
 end
